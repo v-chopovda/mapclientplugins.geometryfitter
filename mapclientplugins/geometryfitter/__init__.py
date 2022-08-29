ae6bcb8d85ee9a9ded0ce1474ec11903241a47b7
hsorby <h.sorby@auckland.ac.nz>
2022-08-30 10:36:51 +1200

    Set version to 0.3.1.

--- mapclientplugins/geometryfitter/__init__.py
+++ mapclientplugins/geometryfitter/__init__.py
@@ -3,7 +3,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Auckland Bioengineering Institute'
 __stepname__ = 'Geometry Fitter'
 __location__ = 'https://github.com/ABI-Software/mapclientplugins.geometryfitter'
